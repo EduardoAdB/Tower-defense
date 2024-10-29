2ee44320a3fb7926f7f8a4255abef46e5942d750
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-10-28 22:25:09 -0300

    level manager comentado :diamond_shape_with_a_dot_inside:

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,42 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class LevelManager : MonoBehaviour
 {
-    public static LevelManager main;
+    public static LevelManager main;  // Singleton para permitir acesso global a esta instância de LevelManager
 
-    public Transform[] path;
-    public Transform startPoint;
+    public Transform[] path;  // Array de pontos que define o caminho que os inimigos devem seguir
+    public Transform startPoint;  // Ponto de início dos inimigos no nível
 
-    public int currency;
+    public int currency;  // Moeda disponível para o jogador
 
     private void Awake()
     {
-        main = this;
+        main = this;  // Define esta instância como a principal do LevelManager
     }
+
     private void Start()
     {
-        currency = 100;
+        currency = 100;  // Inicializa a moeda com 100 unidades
     }
+
     public void IncreaseCurrency(int amount)
     {
-        currency += amount;
+        currency += amount;  // Aumenta a quantidade de moeda do jogador em uma quantidade específica
     }
 
     public bool SpendCurrency(int amount)
     {
-        if (amount <= currency)
+        if (amount <= currency)  // Verifica se o jogador tem moeda suficiente
         {
-            Debug.Log("Comprou");
-            currency -= amount;
-            return true;
+            Debug.Log("Comprou");  // Log para indicar que a compra foi realizada
+            currency -= amount;  // Deduz o valor da compra da moeda
+            return true;  // Retorna verdadeiro para indicar que a compra foi bem-sucedida
         }
         else
         {
-            Debug.Log("Você não tem dinheiro");
-            return false;
+            Debug.Log("Você não tem dinheiro");  // Log para indicar que o jogador não tem moeda suficiente
+            return false;  // Retorna falso para indicar que a compra falhou
         }
-
     }
 }
