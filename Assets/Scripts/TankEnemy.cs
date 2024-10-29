6d8a07c9badc869c5531c43d839eced3a3c2b975
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-10-28 22:42:54 -0300

    TankEnemy comentado

--- Assets/Scripts/TankEnemy.cs
+++ Assets/Scripts/TankEnemy.cs
@@ -2,25 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Classe TankEnemy que herda da classe EnemyMovement
 public class TankEnemy : EnemyMovement
 {
     private void Update()
     {
-        MeiaVida();
-        Updt();
+        MeiaVida();  // Chama o método MeiaVida para verificar se a habilidade deve ser ativada
+        Updt();  // Chama o método Updt da classe pai para atualizar o movimento do inimigo
     }
+
     private void FixedUpdate()
     {
-        FxdUpdate();
+        FxdUpdate();  // Chama o método FxdUpdate da classe pai para atualizar a física do movimento
     }
+
+    // Sobrescreve o método MeiaVida da classe pai
     public override void MeiaVida()
     {
+        // Verifica se os pontos de vida do inimigo estão iguais à metade da vida total e se a habilidade ainda não foi ativada
         if (hitPoints == metadeDaVida && habilidadeAtiva == false)
         {
-            habilidadeAtiva = true;
-            hitPoints = hitPoints * 4;
+            habilidadeAtiva = true;  // Ativa a habilidade
+            hitPoints = hitPoints * 4;  // Dobra os pontos de vida do inimigo
         }
-
     }
 }
-
