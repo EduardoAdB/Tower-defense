dffa8d0a98289734a5b607be9031e40f4eea4750
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-10-28 22:41:38 -0300

    Fire enemy comentado :diamond_shape_with_a_dot_inside:

--- Assets/Scripts/FireEnemy.cs
+++ Assets/Scripts/FireEnemy.cs
@@ -2,26 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Classe FireEnemy que herda da classe EnemyMovement
 public class FireEnemy : EnemyMovement
 {
     private void Update()
     {
-        MeiaVida();
-        Updt();
+        MeiaVida();  // Chama o método MeiaVida para verificar se a habilidade deve ser ativada
+        Updt();  // Chama o método Updt da classe pai para atualizar o movimento do inimigo
     }
+
     private void FixedUpdate()
     {
-        FxdUpdate();
+        FxdUpdate();  // Chama o método FxdUpdate da classe pai para atualizar a física do movimento
     }
+
+    // Sobrescreve o método MeiaVida da classe pai
     public override void MeiaVida()
     {
+        // Verifica se os pontos de vida do inimigo estão iguais à metade da vida total e se a habilidade ainda não foi ativada
         if (hitPoints == metadeDaVida && habilidadeAtiva == false)
         {
-            habilidadeAtiva = true;
-            hitPoints = hitPoints * 2;
-            baseSpeed = 0.5f;  
+            habilidadeAtiva = true;  // Ativa a habilidade
+            hitPoints = hitPoints * 2;  // Dobra os pontos de vida do inimigo
+            baseSpeed = 0.5f;  // Diminui a velocidade base do inimigo
         }
-
     }
-
 }
