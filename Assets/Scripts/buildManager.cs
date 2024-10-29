5c385a845d832e16f5ac6526d49b3d8f3293013e
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-10-28 22:25:35 -0300

    build manager comentado

--- Assets/Scripts/buildManager.cs
+++ Assets/Scripts/buildManager.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 public class buildManager : MonoBehaviour
 {
     #region singleton
-    public static buildManager main;
+    public static buildManager main;  // Cria uma referência estática para permitir acesso global a esta instância de buildManager
     #endregion
 
     [Header("References")]
-    [SerializeField] private Tower[] towers;
-    
-    private int SelectedTower = 0;
+    [SerializeField] private Tower[] towers;  // Array de torres disponíveis para construção
+
+    private int SelectedTower = 0;  // Índice da torre selecionada atualmente
 
     private void Awake()
     {
-        main = this;
+        main = this;  // Define esta instância de buildManager como a instância principal (singleton)
     }
+
     public Tower GetSelectedTower()
     {
-        return towers[SelectedTower];
+        return towers[SelectedTower];  // Retorna a torre atualmente selecionada do array de torres
     }
+
     public void SetSelectedTower(int _selectedTower)
     {
-        SelectedTower = _selectedTower;
+        SelectedTower = _selectedTower;  // Atualiza o índice da torre selecionada com o valor fornecido
     }
 }
