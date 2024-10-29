913bc81357c1eab2002dc04d2a0fe5fc3070b67a
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-10-28 22:25:23 -0300

    Menu comentado

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,19 +6,19 @@ using UnityEngine;
 public class Menu : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] TextMeshProUGUI currencyUI;
-    [SerializeField] Animator anim;
+    [SerializeField] TextMeshProUGUI currencyUI;  // Referência ao elemento de UI que exibe a quantidade de moeda
+    [SerializeField] Animator anim;  // Referência ao componente Animator para controlar animações do menu
 
-    private bool isMenuOpen = true;
+    private bool isMenuOpen = true;  // Indica se o menu está aberto ou fechado
 
     public void ToggleMenu()
     {
-        isMenuOpen = !isMenuOpen;
-        anim.SetBool("Menu Open", isMenuOpen);
+        isMenuOpen = !isMenuOpen;  // Inverte o estado do menu (abre se estiver fechado e vice-versa)
+        anim.SetBool("Menu Open", isMenuOpen);  // Define a animação de abrir ou fechar o menu com base no estado atual
     }
 
     private void OnGUI()
     {
-        currencyUI.text = LevelManager.main.currency.ToString();
+        currencyUI.text = LevelManager.main.currency.ToString();  // Atualiza o texto do UI com a moeda atual do jogador
     }
 }
