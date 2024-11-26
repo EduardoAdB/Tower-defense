ed6ecf2678d42106d4d706aa6ef760861cbf8254
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-11-26 14:57:42 -0300

    Adds extra :diamond_shape_with_a_dot_inside:

--- Assets/Scripts/AdManager.cs
+++ Assets/Scripts/AdManager.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,22 +9,62 @@ public class AdManager : MonoBehaviour, IUnityAdsInitializationListener, IUnityA
     private string gameId = "5735164"; 
     private bool testMode = true;
 
+
     private void Start()
     {
+        Advertisement.Initialize(gameId, testMode);
         Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
         Advertisement.Banner.Show("Rewarded_Android");
         Advertisement.Banner.Show("Banner_Android");
+    }
+    
+    public void MostrarAD()
+    {       
+        Advertisement.Show("Rewarded_Android", this);
+
+        //pausar o jogo
+    }
+
+
+
+
+
+
+
+    public void OnInitializationComplete()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        throw new System.NotImplementedException();
     }
 }
 
 
 
 
-public interface IUnityAdsInitializationListener
-{
 
-}
-public interface IUnityAdsShowListener
-{
 
-}
+
